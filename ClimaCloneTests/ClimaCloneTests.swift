17a7fb453070a5d3d74785e174b6a9084e6b7947
Kevin Flores <kevin22pr@gmail.com>
2025-02-13 15:17:32 -0400

    Base project

---
+++ ClimaCloneTests/ClimaCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaCloneTests.swift
+//  ClimaCloneTests
+//
+//  Created by Kevin Flores on 2/13/25.
+//
+
+import Testing
+@testable import ClimaClone
+
+struct ClimaCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
